8892550f1a0130d274af2fa4d89df74f49b9adfa
juancoloradoa <100529676+juancoloradoa@users.noreply.github.com>
2022-12-23 08:25:13 -0500

    feat(users): add store to the UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\View\View;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class UserController extends Controller
 {
@@ -12,5 +13,10 @@ class UserController extends Controller
         $users = User::all();
 
         return \view('users.index', compact('users'));
+    }
+
+    public function store(): RedirectResponse
+    {
+        return redirect()->route('user.index');
     }
 }
